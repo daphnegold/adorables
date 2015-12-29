28beca2edb635f2ff0a5caea206ff0b78f97d9d9
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-29 16:07:54 -0500

    Add hashtag creation logic to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate, only: [:create]
+  # before_filter :authenticate, only: [:create]
 
   def new
     unless session[:user_id].nil?
@@ -12,8 +12,20 @@ class CommentsController < ApplicationController
 
   def create
     Comment.create(comment_params)
-    Comment.last.update(user_id: session[:user_id])
-
+    Comment.last.update(user_id: 1) #session[:user_id])
+    tags = comment_params[:comment].scan(/\B#\w+/)
+    link = Link.find(params[:link_id])
+    unless tags.empty?
+      tags.each do |tag|
+        existing_hashtag = Hashtag.find_by_tag(tag)
+        if existing_hashtag.nil?
+          new_hashtag = Hashtag.create(tag: tag)
+          new_hashtag.links << link
+        else
+          existing_hashtag.links << link unless link.hashtags.include?(existing_hashtag)
+        end
+      end
+    end
     redirect_to links_path(anchor: "link_#{params[:link_id]}")
   end
 
